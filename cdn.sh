817407eb9ed0582f23f7cd676a16d5cc320f5f51
QiuDog <qiudog825@gmail.com>
2020-04-04 21:35:06 +0800

    trigger travis build

--- cdn.sh
+++ cdn.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 printf "        except "
 curl https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf -s |  while read line; do awk -F '/' '{print $2}' | grep -v '#' ; done |  paste -sd " " -
+
